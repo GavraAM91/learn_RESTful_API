156a9975ff244d23f91ed817e50047c64c0796ce
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-07-12 15:25:53 +0700

    membuat model contact

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -12,4 +13,8 @@ class User extends Model
     protected $KeyType = "int";
     protected $timestamps = true;
     protected $incrementing = true;
+
+    public function contacts(): HasMany {
+        return $this->hasMany(Contact::class, "user_id", "id");
+    }
 }
